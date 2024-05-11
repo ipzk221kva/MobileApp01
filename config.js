e6734c3afa014d09d2e1215076a0e34629cf76d4
VladKuzmin <ipzk221_kva@student.ztu.edu.ua>
2024-05-12 01:04:12 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const Config = {
+    newsUrl: 'https://raw.githubusercontent.com/ipzk221kva/MobileApp1/main/news/news.json',
+    galleryUrl: 'https://raw.githubusercontent.com/ipzk221kva/MobileApp1/main/images.json',
+};
+
+export default Config;
